41e7986f9cc747873628239b98efc2dfa73c2e36
gurayorhan10 <gurayorhan10@gmail.com>
2025-04-01 22:45:30 +0300

    crawler

--- src/main/java/com/project/crawlerservice/entity/ExchangeRateEntity.java
+++ src/main/java/com/project/crawlerservice/entity/ExchangeRateEntity.java
@@ -24,9 +24,9 @@ public class ExchangeRateEntity {
     private Currency currency;
     @Column(name = "NAME", nullable = false, length = 250)
     private String name;
-    @Column(name = "BUY", nullable = false, precision = 5, scale = 5)
+    @Column(name = "BUY", nullable = false, precision = 12, scale = 5)
     private BigDecimal buy;
-    @Column(name = "SELL", nullable = false, precision = 5, scale = 5)
+    @Column(name = "SELL", nullable = false, precision = 12, scale = 5)
     private BigDecimal sell;
     @Column(name = "LAST_UPDATE_DATE", nullable = false)
     private Date lastUpdateDate;
